1eb6f2cd043285f3bd434e22c32b36c0052f44b3
Bence Balogh <benceeok@gmail.com>
2022-01-07 23:11:22 +0100

    update setup process

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dewloosh.math
 
+**The package in alpa stage, under development**. Expected date of the first stable release is 2022-Q2.
--- src/dewloosh/math/__init__.py
+++ src/dewloosh/math/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.0.dev6"
+__version__ = "0.0.dev7"
 from dewloosh.math.hist import *
+__description__ = "A math library for numerical calculations."
