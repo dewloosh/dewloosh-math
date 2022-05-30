3a81df5fc9672b82e7315c43205e28c43693c4e5
Bence Balogh <benceeok@gmail.com>
2022-05-30 20:55:20 +0200

    version update

--- src/dewloosh/math/__init__.py
+++ src/dewloosh/math/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.0.1-alpha5"
+__version__ = "0.0.1-alpha6"
 from dewloosh.math.hist import *
 from dewloosh.math.utils import *
 __description__ = "A math library for numerical calculations."
