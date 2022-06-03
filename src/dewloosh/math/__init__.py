41e2e4a9a92b567a7ef93f7414a1e439bf7dd9f4
Bence Balogh <benceeok@gmail.com>
2022-06-03 22:05:44 +0200

    Update __init__.py

--- src/dewloosh/math/__init__.py
+++ src/dewloosh/math/__init__.py
@@ -1,5 +1,43 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.0.1-alpha6"
+from typing import Callable
+import numpy as np
+
 from dewloosh.math.hist import *
 from dewloosh.math.utils import *
+
+__version__ = "0.0.1-alpha6"
 __description__ = "A math library for numerical calculations."
+
+
+def squeeze_if_array(arr): return np.squeeze(
+    arr) if isinstance(arr, np.ndarray) else arr
+
+
+def squeeze(default=True):
+    def decorator(fnc: Callable):
+        def inner(*args, **kwargs):
+            if kwargs.get('squeeze', default):
+                res = fnc(*args, **kwargs)
+                if isinstance(res, tuple):
+                    return list(map(squeeze_if_array, res))
+                return squeeze_if_array(res)
+            else:
+                return fnc(*args, **kwargs)
+        return inner
+    return decorator
+
+
+def config(*args, **kwargs):
+    def decorator(fnc: Callable):
+        def inner(*args, **kwargs):
+            return fnc(*args, **kwargs)
+        return inner
+    return decorator
+
+
+def is_none_or_false(a):
+    if isinstance(a, bool):
+        return not a
+    elif a is None:
+        return True
+    return False
