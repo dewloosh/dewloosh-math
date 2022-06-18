ddf2a58758333641d9318a299fd3c1f224577527
Bence Balogh <benceeok@gmail.com>
2022-06-18 21:28:45 +0200

    maintenance

--- tests/test_lpp.py
+++ tests/test_lpp.py
@@ -2,7 +2,7 @@
 import unittest
 import numpy as np
 
-from dewloosh.math.function import Function, Equality
+from dewloosh.math.function import Function, Equality, InEquality
 from dewloosh.math.optimize import LinearProgrammingProblem as LPP, \
     DegenerateProblemError, NoSolutionError
 import sympy as sy
@@ -26,7 +26,7 @@ class TestLPP(unittest.TestCase):
         x = P1.solve(order=[0, 2, 3, 1], raise_errors=True)['x']
         _x = np.array([0., 6., 0., 4.])
         assert np.all(np.isclose(_x, x))
-        
+    
     """def test_degenerate_solution(self):
         variables = ['x1', 'x2', 'x3', 'x4']
         x1, x2, x3, x4 = syms = sy.symbols(variables, positive=True)
@@ -89,6 +89,26 @@ class TestLPP(unittest.TestCase):
         lpp = LPP(cost=f, constraints=[eq], variables=syms)
         x = lpp.solve(return_all=True, raise_errors=True)['x']
         assert np.all(np.isclose(x, np.array([0., 2.])))
+        
+    def test_2(self):
+        x1, x2 = sy.symbols(['x1', 'x2'], positive=True)
+        syms = [x1, x2]
+        f = Function(x1 + x2, variables=syms)
+        ieq1 = InEquality(x1 - 1, op='>=', variables=syms)
+        ieq2 = InEquality(x2 - 1, op='>=', variables=syms)
+        ieq3 = InEquality(x1 + x2 - 4, op='<=', variables=syms)
+        lpp = LPP(cost=f, constraints=[ieq1, ieq2, ieq3], variables=syms)
+        x = lpp.solve(return_all=True, raise_errors=True)['x']
+        assert np.all(np.isclose(x, np.array([1., 1.])))
+        x1, x2 = sy.symbols(['x1', 'x2'], positive=True)
+        syms = [x1, x2]
+        f = Function(x1 + x2, variables=syms)
+        ieq1 = InEquality(x1 - 1, op='>=', variables=syms)
+        ieq2 = InEquality(x2 - 1, op='>=', variables=syms)
+        ieq3 = InEquality(x1 + x2 - 4, op='>=', variables=syms)
+        lpp = LPP(cost=f, constraints=[ieq1, ieq2, ieq3], variables=syms)
+        e = lpp.solve(return_all=True, raise_errors=True, as_dict=True)['e']
+        assert len(e) == 0
     
     
 if __name__ == "__main__":
