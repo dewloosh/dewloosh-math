446fe7a696386e8602f6edd4e3d3dcf9e866a02b
Bence Balogh <benceeok@gmail.com>
2022-06-19 21:12:50 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,12 @@
 #
 import subprocess
 import os
-# import sys
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 # -- Project information -----------------------------------------------------
 
