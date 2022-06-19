295c0ae6978aa647d7235af2f4d24a57d813c3a8
Bence Balogh <benceeok@gmail.com>
2022-06-19 21:01:56 +0200

    Update test_interval.py

--- tests/test_interval.py
+++ tests/test_interval.py
@@ -54,11 +54,11 @@ class TestInterval(unittest.TestCase):
 if __name__ == "__main__":
 
     # small example to compile functions
-    lo = 1.0
+    """lo = 1.0
     hi = 3.0
     data = np.array([1.1, 3.1, 2.1])
     new_interval(lo, hi, data)._arr
     interval_data(new_interval(lo, hi, data))
-    interval_getitem(new_interval(lo, hi, data), 0)
+    interval_getitem(new_interval(lo, hi, data), 0)"""
 
     #unittest.main()
